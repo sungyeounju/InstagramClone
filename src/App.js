a09a1703d48ad581ffb92d8b4ead32f517dd4c03
sungyeounju <yj0606yj.naver.com>
2023-03-05 21:27:10 +0900

    update : 구조 셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Route} from 'react-router-dom';
+// import Nav from './Nav.js';
+function Nav(){
+  return (
+  <div>하 존나 막막하네 여기는 NAV 자리</div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {/* <Route>
+      <Nav></Nav>
+    </Route> */}
+    
+    <Nav></Nav>
+    <div>프로필들어갈것임</div>
+    <div>팔로우 팔로잉 자리</div>
+    <div>여기는 콘텐츠자리</div>
+    <div>여기는 메뉴인데 맨 위로 올려야할까</div>
+
     </div>
   );
 }
